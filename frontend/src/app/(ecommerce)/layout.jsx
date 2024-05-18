ea667e0be3491220e56a3a6ae12e43e9c009c26e
Ramon Francisco <ramon.francisco.silva99@gmail.com>
2024-05-18 18:38:20 -0300

    chore: adjustment layouts

--- frontend/src/app/(ecommerce)/layout.jsx
+++ frontend/src/app/(ecommerce)/layout.jsx
@@ -1,12 +1,16 @@
 import Header from "../components/header";
 import styles from "@/app/styles/container.module.scss";
 import MainStyles from "@/app/styles/Main.module.scss";
+import Breadcrumb from "../components/breadcrumb";
 
 export default function EcommerceLayout({ children }) {
   return (
     <div className={styles.container}>
       <Header />
-      <main className={MainStyles.main}>{children}</main>
+      <main className={MainStyles.main}>
+        <Breadcrumb />
+        {children}
+      </main>
     </div>
   );
 }
