339ecdab382dbf7724fc01fa191f1098d7d32cc5
Ramon Francisco <ramon.francisco.silva99@gmail.com>
2024-05-20 08:21:00 -0300

    vercel deploy

--- bff/bin/server.ts
+++ bff/bin/server.ts
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3333;
 
 (async () => {
-  const { setupApp } = await import("../api");
+  const { setupApp } = await import("../src");
   const app = setupApp();
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
