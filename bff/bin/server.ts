ce81892752c91a359a61fd33d123e63c41821399
Ramon Francisco <ramon.francisco.silva99@gmail.com>
2024-05-20 08:13:32 -0300

    rename src

--- bff/bin/server.ts
+++ bff/bin/server.ts
@@ -1,7 +1,7 @@
 const PORT = process.env.PORT || 3333;
 
 (async () => {
-  const { setupApp } = await import("../src/index");
+  const { setupApp } = await import("../api");
   const app = setupApp();
   app.listen(PORT, () => {
     console.log(`App running on port ${PORT}`);
